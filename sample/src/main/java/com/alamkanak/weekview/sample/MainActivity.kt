91ab45fc76a9517962249a566e9b12ddea6b0393
AndroidDeveloperLB <lblb636@gmail.com>
2019-02-08 08:24:18 +0200

    updated sdks

--- sample/src/main/java/com/alamkanak/weekview/sample/MainActivity.kt
+++ sample/src/main/java/com/alamkanak/weekview/sample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.alamkanak.weekview.sample
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,5 +27,26 @@ class MainActivity : AppCompatActivity() {
         }
 //        startActivity(Intent(this@MainActivity, WholeViewSnappingActivity::class.java))
 //        finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var url: String? = null
+        when (item.itemId) {
+            R.id.menuItem_all_my_apps -> url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB"
+            R.id.menuItem_all_my_repositories -> url = "https://github.com/AndroidDeveloperLB"
+            R.id.menuItem_current_repository_website -> url = "https://github.com/AndroidDeveloperLB/Android-Week-View"
+        }
+        if (url == null)
+            return true
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        startActivity(intent)
+        return true
     }
 }
